f5411df30cd7d0e979ef150090d6d946ea6a80b4
behdad <behdad@5b36427b-0638-4ae6-a458-79a945e7ecfe>
2005-08-05 19:45:10 +0000

    Added.  Empty stubs actually.
    git-svn-id: http://rira.svn.sourceforge.net/svnroot/rira/trunk@10 5b36427b-0638-4ae6-a458-79a945e7ecfe

---
+++ rira/php/module/children/home.php
@@ -0,0 +1,10 @@
+<?php
+
+class children__home extends __rira_default_home {
+  var $default_title = "کودکان";
+  var $default_long_title = "کتاب کودکان";
+  var $child = 'book';
+  var $searchable = true;
+}
+
+?>
---
+++ rira/php/module/contemporarypoems/home.php
@@ -0,0 +1,10 @@
+<?php
+
+class contemporarypoems__home extends __rira_default_home {
+  var $default_title = "شعر نو";
+  var $default_long_title = "شاعران معاصر";
+  var $child = 'book';
+  var $searchable = true;
+}
+
+?>
