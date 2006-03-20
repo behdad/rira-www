b6f4985248bffc4cb29d108204576956acb3abd1
behdad <behdad@5b36427b-0638-4ae6-a458-79a945e7ecfe>
2006-03-20 21:46:33 +0000

    Make customizing easier.
    git-svn-id: http://rira.svn.sourceforge.net/svnroot/rira/trunk@24 5b36427b-0638-4ae6-a458-79a945e7ecfe

--- rira/php/script/include.php
+++ rira/php/script/include.php
@@ -1,8 +1,14 @@
 <?php
 
 define('BASE', dirname(__FILE__).'/../');
+$custom_path = dirname (__FILE__).'/custom/';
+set_include_path($custom_path.PATH_SEPARATOR.get_include_path());
 
 include_once BASE."script/config.php";
+$custom_config = "$custom_path/config.php";
+if (file_exists ($custom_config))
+  include_once $custom_config;
+
 include_once BASE."script/init.php";
 include_once BASE."script/tools.php";
 include_once BASE."script/persian.php";
@@ -11,5 +17,9 @@ include_once BASE."script/engine.php";
 include_once BASE."script/search.php";
 include_once BASE."script/query.php";
 include_once BASE."script/rira_objs.php";
+
+$custom_include = "$custom_path/include.php";
+if (file_exists ($custom_include))
+  include_once $custom_include;
 
 ?>
