006928fa339212bea35ccb8583bc4b779ff4ec6a
behdad <behdad@5b36427b-0638-4ae6-a458-79a945e7ecfe>
2006-03-20 22:01:14 +0000

    Compress output.
    git-svn-id: http://rira.svn.sourceforge.net/svnroot/rira/trunk@26 5b36427b-0638-4ae6-a458-79a945e7ecfe

--- rira/php/script/init.php
+++ rira/php/script/init.php
@@ -8,4 +8,8 @@ ini_set ('display_errors', $cfg['php_debug'] ? 1 : 0);
 
 setlocale(LC_ALL, $cfg['locale']);
 
+/* compress body */
+if (!ini_get("zlib.output_compression") && ini_get("output_handler") != "ob_gzhandler")
+  ob_start("ob_gzhandler");
+
 ?>
