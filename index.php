3882d6ac3fe64ed5748bdc304a47237e8007a717
Behdad Esfahbod <behdad@behdad.org>
2013-03-10 20:18:51 -0400

    Move ./rira/php/* to ./
    Remove two directory levels.
    
    Adds two symlinks named rira and php, such that old URLs that
    referred to /rira/php/? continue to work.

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-  header("Location: rira/php?".$_SERVER["QUERY_STRING"]);
+  require_once "script/include.php";
+
+  get_page();
+
+  {
+    $s = '';
+    foreach (array ('css', 'js') as $what)
+      foreach (array ('site', 'module') as $where)
+        foreach (array ('main', $page) as $which)
+	{
+          $f = "${where}_${what}";
+          $s .= $f($which);
+        }
+    $s .= www_css("css.php?mod=$mod&bg=".@$modules[$mod]['bg_color']);
+    $s .= www_js("http://behdad.org/js/isiri2901.js");
+    $header = "$s$header";
+  }
+
+  $onload .= $o->get_onload();
+
+  include 'template/main.php';
 ?>
