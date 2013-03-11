fc3e82907d81d236f97851296e0e4d8f38cb63ef
Behdad Esfahbod <behdad@behdad.org>
2013-03-10 22:40:34 -0400

    Do proper Quran formatting
    Looks delicious!

--- module/quran/sura.php
+++ module/quran/sura.php
@@ -8,6 +8,26 @@ class quran__sura extends __rira_default_obj {
   var $nocascade = true;
   var $searchable = true;
   var $searchindexed = true;
+
+  function body_begin_format ()
+  {
+    return '<div class="sura">'."\n";
+  }
+
+  function body_row_format ($contents, $row_num = '')
+  {
+    $s = $contents;
+    if ($row_num)
+      $s .= '&#x06DD;'.localized_number($row_num);
+    $s .= "\n";
+    return $s;
+  }
+
+  function body_end_format ()
+  {
+    return '</div>'."\n";
+  }
+
 }
 
 ?>
